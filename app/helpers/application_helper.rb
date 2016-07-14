c3caaaa0a7b4339b2e4c6d7a99e0e49e452d47f9
sherifkhalil <eng_sherifkhalil@hotmail.com>
2016-07-14 16:56:31 +0200

    adding sign_up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+	def full_title(title)
+		base_title = "Pages Base Title | "
+		page_title =  nil
+		if title.empty?
+			page_title = base_title
+		else
+			page_title = "#{base_title}#{ title}"
+		end  
+	end
+
+	def play
+		"Hello, every body"
+	end
 end
